d43dfc81432f10ec65ca16fae04207cfbe60fa7e
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-11 19:41:28 +0100

    Redundant object

--- TomLonghurst.Selenium.PlaywrightWebDriver/PlaywrightDriverOptions.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Playwright;
-
-namespace TomLonghurst.Selenium.PlaywrightWebDriver;
-
-public record PlaywrightDriverOptions
-{
-    public bool Headless { get; set; } = true;
-    public string BrowserType { get; set; } = Microsoft.Playwright.BrowserType.Chromium;
-};

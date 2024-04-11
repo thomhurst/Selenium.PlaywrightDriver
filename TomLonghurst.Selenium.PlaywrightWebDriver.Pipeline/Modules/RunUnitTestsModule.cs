4268a4910d586f2cb6d812395b21e1d63ff62a6b
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-04-11 21:49:41 +0100

    Run tests in both V3 and V4 mode

--- TomLonghurst.Selenium.PlaywrightWebDriver.Pipeline/Modules/RunUnitTestsModule.cs
+++ TomLonghurst.Selenium.PlaywrightWebDriver.Pipeline/Modules/RunUnitTestsModule.cs
@@ -3,8 +3,10 @@ using ModularPipelines.DotNet.Extensions;
 using ModularPipelines.DotNet.Options;
 using ModularPipelines.Git.Extensions;
 using ModularPipelines.Enums;
+using ModularPipelines.Extensions;
 using ModularPipelines.Models;
 using ModularPipelines.Modules;
+using File = ModularPipelines.FileSystem.File;
 
 namespace TomLonghurst.Selenium.PlaywrightWebDriver.Pipeline.Modules;
 
@@ -12,20 +14,35 @@ public class RunUnitTestsModule : Module<List<CommandResult>>
 {
     protected override async Task<List<CommandResult>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
     {
-        var results = new List<CommandResult>();
-
-        foreach (var unitTestProjectFile in context
-                     .Git().RootDirectory!
-                     .GetFiles(file => file.Path.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase)
-                                       && file.NameWithoutExtension.EndsWith(".Tests", StringComparison.OrdinalIgnoreCase)))
+        var projectFile = context.Git()
+            .RootDirectory
+            .AssertExists()
+            .FindFile(x => x.Name == "TomLonghurst.Selenium.PlaywrightWebDriver.Tests.csproj")
+            .AssertExists();
+        
+        var results = new List<CommandResult>
         {
-            results.Add(await context.DotNet().Test(new DotNetTestOptions
-            {
-                ProjectSolutionDirectoryDllExe = unitTestProjectFile.Path,
-                CommandLogging = CommandLogging.Input | CommandLogging.Error,
-            }, cancellationToken));
-        }
+            await Run(context, cancellationToken, projectFile, true),
+            await Run(context, cancellationToken, projectFile, false)
+        };
 
         return results;
+    }
+
+    private static async Task<CommandResult> Run(IPipelineContext context, CancellationToken cancellationToken,
+        File unitTestProjectFile, bool isV3)
+    {
+        var dotNetTestOptions = new DotNetTestOptions
+        {
+            ProjectSolutionDirectoryDllExe = unitTestProjectFile.Path,
+            CommandLogging = CommandLogging.Input | CommandLogging.Error,
+        };
+
+        if (isV3)
+        {
+            dotNetTestOptions.Properties = new[] { new KeyValue("SeleniumVersion", "3") };
+        }
+        
+        return await context.DotNet().Test(dotNetTestOptions, cancellationToken);
     }
 }
